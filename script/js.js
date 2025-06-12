e64e55ccdc0a97722f21c415fb590a2ed96464cc
Anna De Lise <anna_delise@libero.it>
2025-06-12 15:38:39 +0200

    add changes

--- script/js.js
+++ script/js.js
@@ -1,10 +1,25 @@
-//creo una funzione che generi numeri random da 1 a 90 escluso lo 0
+//creo una funzione che generi numeri random da 1 a 76 escluso lo 0
 const getRandomNum = () => {
-  return Math.floor(Math.random() * 90) + 1;
+  return Math.floor(Math.random() * 75) + 1;
 };
 
 // creo un array vuoto dove inserire i numeri estratti
 const arrNum = [];
+
+// creo un tabellone con 76 celle
+// prendo il riferimento dove crearlo
+const container = document.getElementById("container");
+//ciclo la produzione di 76 celle
+for (let i = 0; i < 76; i++) {
+  //creo una cella
+  const cella = document.createElement("div");
+  //assegno ad ogni cella una classe
+  cella.classList.add("currCella");
+  //assegno ad ogni cella un numero
+  cella.innerText = i + 1;
+  //appendo cella al contenitore
+  container.appendChild(cella);
+}
 
 //prendo il bottone dall html to Js
 const btn = document.querySelector("button");
@@ -16,5 +31,10 @@ btn.addEventListener("click", () => {
   if (!arrNum.includes(num)) {
     arrNum.push(num);
   }
-  console.log(arrNum);
+
+  //arrNum[num-1] ritorna un numero che può essere usato come posizione per l'array di celle
+  //trovo tutte le celle e mi faccio ridare indietro una NodeList
+  const arrCelle = document.querySelectorAll(".currCella");
+  //seleziono le celle che hanno [num-1], ossia i numeri usciti, e aggiungo un ulteriore classe "selezionate"
+  arrCelle[num - 1].classList.add("selezionate");
 });
